00767b08e4b3218e3f0cf2c586e0196a5216e367
zeroti <guodaipi@email.com>
2023-04-12 20:54:34 +0800

    添加后台管理

---
+++ hexo-deploy.sh
@@ -0,0 +1,10 @@
+###
+ # @Author: Zhao
+ # @Date: 2023-04-11 17:22:22
+ # @LastEditTime: 2023-04-11 17:22:28
+ # @LastEditors: your name
+ # @Description: 自动执行build和部署
+ # @FilePath: \hexo\hexo-deploy.sh
+### 
+
+hexo g && hexo d
